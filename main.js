f3b67c1a3cac3b43abd61881cc090424bab63c92
Danny <danny05062002@gmail.com>
2024-06-02 16:13:11 +0200

    added a color picker option to closed, open and found cards

--- main.js
+++ main.js
@@ -20,4 +20,32 @@ document.addEventListener('DOMContentLoaded', function() {
         cardPairs(selectedValue);
         startTimer();
     });
+
+    const closedColorInput = document.querySelector('#closedColor');
+    const openColorInput = document.querySelector('#openColor');
+    const foundColorInput = document.querySelector('#foundColor');
+
+    closedColorInput.addEventListener('change', function() {
+        const closedCards = document.querySelectorAll('.card#closed');
+        closedCards.forEach(card => {
+            card.style.backgroundColor = this.value;
+            card.style.borderColor = this.value;
+        });
+    });
+
+    openColorInput.addEventListener('change', function() {
+        const openCards = document.querySelectorAll('.card#open');
+        openCards.forEach(card => {
+            card.style.backgroundColor = this.value;
+            card.style.borderColor = this.value;
+        });
+    });
+
+    foundColorInput.addEventListener('change', function() {
+        const foundCards = document.querySelectorAll('.card#found');
+        foundCards.forEach(card => {
+            card.style.backgroundColor = this.value;
+            card.style.borderColor = this.value;
+        });
+    });
 });
